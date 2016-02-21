f096c95e4f9b682bf0a09aaed796918c352c8785
岩本 一樹 <iwamotokazuki@iwamoto-no-MacBook-Pro.local>
2016-02-21 19:51:11 +0900

    マイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'top/index'
+  get 'users/show'
 
-  root to: "top#index"
+  devise_for :users
+  resources  :users, only: [:show]
+  root       "top#index"
+  get        "top/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
